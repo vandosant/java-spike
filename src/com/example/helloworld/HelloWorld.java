e66c5b40a51c62deb605048af859d5a55db43624
vandosant <sjskender@gmail.com>
2014-07-19 22:05:49 -0600

    Initial commit

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+/**
+ * Created by sskender on 7/19/14.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
